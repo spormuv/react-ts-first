cdab1d4f19f3b58c3d42b164b70edd2a988e9b69
Mike <spormuv@gmail.com>
2023-03-17 17:52:48 +0300

    feat: start first react project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <h1>Hello React!!!</h1>;
+  return e('div', { className: 'container' }, [
+    e('h1', { className: 'font-bold', key: 1 }, 'Test JSX'),
+    e('button', { className: '', key: 2 }, 'click me'),
+  ]);
 }
 
 export default App;
